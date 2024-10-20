3fbf08f1c99ab895f16c55ff67dca5f6223cf956
ShaylinThadani <74334822+ShaylinThadani@users.noreply.github.com>
2024-10-20 00:55:38 -0400

    added all the code

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,20 +1,21 @@
-import {Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AbstractControl, FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
-import {EmployeeService} from "../service/employee.service";
+import { EmployeeService } from "../service/employee.service";
 import { Router, RouterLink } from "@angular/router";
-import {Employee} from "../model/employee";
+import { Employee } from "../model/employee";
 
 @Component({
-    selector: 'app-employee',
-    templateUrl: './employee.component.html',
-    styleUrls: ['./employee.component.css'],
-    standalone: true,
-    imports: [RouterLink, ReactiveFormsModule]
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule]
 })
 export class EmployeeComponent {
   private builder: FormBuilder = inject(FormBuilder);
   private employeeService: EmployeeService = inject(EmployeeService);
   private router: Router = inject(Router);
+  
   employeeForm = this.builder.group({
     name: ['', Validators.required],
     dateOfBirth: ['', Validators.required],
@@ -24,22 +25,29 @@ export class EmployeeComponent {
     email: ['', Validators.email]
   });
 
-  get name(): AbstractControl<string> {return <AbstractControl<string>>this.employeeForm.get('name'); }
-  get dateOfBirth(): AbstractControl<string> {return <AbstractControl<string>>this.employeeForm.get('dateOfBirth'); }
-  get city(): AbstractControl<string> {return <AbstractControl>this.employeeForm.get('city'); }
-  get salary(): AbstractControl<number> {return <AbstractControl<number>>this.employeeForm.get('salary'); }
-  get gender(): AbstractControl<string> {return <AbstractControl<string>>this.employeeForm.get('gender'); }
-  get email(): AbstractControl<string> {return <AbstractControl<string>>this.employeeForm.get('email'); }
+
+
+  get name(): AbstractControl<string> { return this.employeeForm.get('name') as AbstractControl<string>; }
+  get dateOfBirth(): AbstractControl<string> { return this.employeeForm.get('dateOfBirth') as AbstractControl<string>; }
+  get city(): AbstractControl<string> { return this.employeeForm.get('city') as AbstractControl<string>; }
+  get salary(): AbstractControl<number> { return this.employeeForm.get('salary') as AbstractControl<number>; }
+  get gender(): AbstractControl<string> { return this.employeeForm.get('gender') as AbstractControl<string>; }
+  get email(): AbstractControl<string> { return this.employeeForm.get('email') as AbstractControl<string>; }
 
   onSubmit() {
-    const employee: Employee = new Employee(this.name.value,
+    const employee: Employee = new Employee(
+      this.name.value,
       new Date(this.dateOfBirth.value),
       this.city.value,
       this.salary.value,
       this.gender.value,
-      this.email.value);
-    this.employeeService.addEmployee(employee);
-    this.employeeForm.reset();
-    this.router.navigate(['/employees']).then(() => {});
+      this.email.value
+    );
+    this.employeeService.addEmployee(employee).then(() => {
+      this.employeeForm.reset();
+      this.router.navigate(['/employees']);
+    }).catch(error => {
+      console.error('Error adding employee:', error);
+    });
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase : {
+    apiKey: "AIzaSyBVSBUZ_fC8N7_OU8hmyU2LMZv_KdxitsY",
+    authDomain: "lab5-e6dde.firebaseapp.com",
+    projectId: "lab5-e6dde",
+    storageBucket: "lab5-e6dde.appspot.com",
+    messagingSenderId: "916199573342",
+    appId: "1:916199573342:web:2f95812a8daa2bbbfa71cf",
+    measurementId: "G-QXE27LERWG"
+  }};
